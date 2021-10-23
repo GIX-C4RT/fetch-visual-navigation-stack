97f37b05f585df3cf664cdb18ad3b621cf17ffd1
Dewey <deweyfang@gmail.com>
2021-10-23 13:53:32 -0700

    adding the fixed transform

--- odom_map_bind.py
+++ odom_map_bind.py
@@ -33,15 +33,17 @@ while not rospy.is_shutdown():
     try:
         (trans1,rot1) = listener.lookupTransform('/map', '/camera_pose_frame', rospy.Time(0))
         (trans2,rot2) = listener.lookupTransform('/odom', '/base_link', rospy.Time(0))
+        transform_fixed = concatenate_matrices(translation_matrix([-0.2,0,0]), quaternion_matrix([0,0,0,1]))
         # global_trans = [trans1[0] - trans2[0], trans1[1] - trans2[1], 0]
         transform1 = concatenate_matrices(translation_matrix(trans1), quaternion_matrix(rot1))
         transform2 = concatenate_matrices(translation_matrix(trans2), quaternion_matrix(rot2))
-        global_trans_matrix = np.matmul(transform1, np.linalg.inv(transform2))
+        global_trans_matrix = np.matmul(np.matmul(transform1, transform_fixed), np.linalg.inv(transform2))
+        # global_trans_matrix = np.matmul(transform1, np.linalg.inv(transform2))
         global_trans_quaternion = quaternion_from_matrix(global_trans_matrix)
         global_trans_translation = translation_from_matrix(global_trans_matrix)
         br.sendTransform(global_trans_translation,
                     global_trans_quaternion,
-                    rospy.Time.now() + rospy.Duration(1.0),
+                    rospy.Time.now() + rospy.Duration(0.1),
                     "odom",
                     "map")
     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
